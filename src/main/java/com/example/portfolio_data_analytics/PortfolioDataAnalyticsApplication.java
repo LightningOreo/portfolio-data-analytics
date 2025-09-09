b703cbc6f8be670056bc15a1f36ace790348a51c
kiet.nguyen <tknguyen.1202@gmail.com>
2025-09-09 09:28:44 +0700

    Khoi tao du an Spring Boot

---
+++ src/main/java/com/example/portfolio_data_analytics/PortfolioDataAnalyticsApplication.java
@@ -0,0 +1,11 @@
+package com.example.portfolio_data_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioDataAnalyticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioDataAnalyticsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/portfolio_data_analytics/PortfolioDataAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolio_data_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioDataAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
